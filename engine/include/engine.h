dda5f13213ab5231eb1ee00fba2fe26e50d4d22b
Hiep Chan <hiep.lchan@gmail.com>
2025-07-15 21:59:41 +0700

    fix sdl.h main using forward declaration

--- engine/include/engine.h
+++ engine/include/engine.h
@@ -2,8 +2,13 @@
 
 #include "engine_export.h"
 
+struct SDL_Window;
+
 class ENGINE_EXPORT Engine {
 public:
     Engine();
     ~Engine();
+
+private:
+    SDL_Window* windowHandle = nullptr;
 };
--- engine/src/engine.cpp
+++ engine/src/engine.cpp
@@ -8,18 +8,25 @@
 Engine::Engine() {
     std::cout << "Engine::Engine()" << std::endl;
 
-    SDL_version sdlVersion;
-    SDL_VERSION(&sdlVersion);
-    std::cout << "SDL2 version: "
-                << static_cast<int>(sdlVersion.major) << "."
-                << static_cast<int>(sdlVersion.minor) << "."
-                << static_cast<int>(sdlVersion.patch) << std::endl;
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init failed: " << SDL_GetError() << std::endl;
+        return;
+    }
 
-    std::cout << "bgfx Vendor ID (still not init yet): " 
-                << static_cast<int>(bgfx::getCaps()->vendorId) 
-                << std::endl;
+    windowHandle = SDL_CreateWindow("Engine Window", 
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
+        800, 600, SDL_WINDOW_SHOWN || SDL_WINDOW_ALLOW_HIGHDPI);
+
+    if (!windowHandle) {
+        std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << std::endl;
+        return;
+    }
 }
 
 Engine::~Engine() {
     std::cout << "Engine::~Engine()" << std::endl;
+
+    SDL_DestroyWindow(windowHandle);
+    SDL_Quit();
+    windowHandle = nullptr;
 }
