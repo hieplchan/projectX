288545d11f1ec54ad12924e3ed428039c4a1873a
hiep.lchan <hiep.lchan@gmail.com>
2025-11-22 22:07:28 +0700

    can buildFactoryFuncMap from COMPONENT_LIST(REGISTER_COMPONENT_TYPE)

--- engine/main/ComponentFactory.cpp
+++ engine/main/ComponentFactory.cpp
@@ -1,9 +1,35 @@
+#include <Logger.h>
 #include <ComponentFactory.h>
 
 namespace {
+ComponentFactoryFuncMap buildFactoryFuncMap() {
+    ComponentFactoryFuncMap factoryMap;
 
+    #define REGISTER_COMPONENT_TYPE(Type) \
+    factoryMap.try_emplace(#Type, [](const json& jData) -> ComponentPtr { \
+        auto obj = std::make_unique<Type>(); \
+        populateCompFromJson<Type>(obj.get(), jData); \
+        return obj; \
+    });
+
+    COMPONENT_LIST(REGISTER_COMPONENT_TYPE)
+    #undef REGISTER_COMPONENT_TYPE
+
+    return factoryMap;
+}
 } // anonymous namespace
 
-std::unique_ptr<Component> createCompFromJson(std::string_view typeName, const nlohmann::json& jData) {
+ComponentPtr createCompFromJson(std::string_view typeName, const json& jData) {
+    static const ComponentFactoryFuncMap factoryMap = buildFactoryFuncMap();
+    if (auto it = factoryMap.find(typeName); it != factoryMap.end()) {
+        return it->second(jData);
+    }
+
+    LOG_ERROR("ComponentFactory: Unknown component type {}", typeName);
     return nullptr;
+}
+
+template<ComponentType T>
+void populateCompFromJson(T* comp, const json& jData) {
+
 }
--- engine/main/ComponentFactory.h
+++ engine/main/ComponentFactory.h
@@ -1,9 +1,26 @@
 #pragma once
 
 #include <memory>
+#include <functional>
+#include <string_view>
+#include <unordered_map>
 #include <nlohmann/json.hpp>
 
 #include "ComponentBase.h"
-#include "ComponentTypes.h"
+#include "ComponentList.h"
 
-std::unique_ptr<Component> createCompFromJson(std::string_view typeName, const nlohmann::json& jData);
+using json = nlohmann::json;
+using ComponentPtr = std::unique_ptr<Component>;
+using ComponentFactoryFunc = std::function<ComponentPtr(const json&)>;
+using ComponentFactoryFuncMap = std::unordered_map<std::string_view, ComponentFactoryFunc>;
+
+ComponentPtr createCompFromJson(
+    std::string_view typeName,
+    const json& jData
+);
+
+template<ComponentType T>
+void populateCompFromJson(
+    T* comp,
+    const json& jData
+);
