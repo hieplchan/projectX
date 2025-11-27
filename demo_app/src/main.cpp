98aef3f96c7f811d4ef9d89dacc56bc7bdcf04ea
hiep.lchan <hiep.lchan@gmail.com>
2025-11-27 22:34:41 +0700

    can load Camera from scene1.json

--- demo_app/src/main.cpp
+++ demo_app/src/main.cpp
@@ -16,31 +16,6 @@ int main() {
 
     LOG_INFO(std::format("Demo app started at: {}", std::chrono::system_clock::now()));
 
-    // json jTest = {"key", "value"};
-    // std::string out = jTest.dump();
-    // LOG_INFO(std::format("JSON test output: {}", out));
-    // std::vector<uint8_t> cborTest = json::to_cbor(jTest);
-    // std::string cborTestStr(cborTest.begin(), cborTest.end());
-    // LOG_INFO(std::format("CBOR test output: {}", cborTestStr));
-
-    auto cameraGO = std::make_unique<GameObject>();
-    auto cameraGOTF = cameraGO->addComponent<Transform>();
-    auto cameraGOCam = cameraGO->addComponent<Camera>();
-    cameraGO->setName("Main Camera");
-    cameraGOTF->position = glm::vec3{ 0.0f, 0.0f, -10.0f };
-    cameraGOCam->fovDeg = 30.0f;
-    engine.addGameObject(std::move(cameraGO));
-
-    // // Create simple game object with QuadRenderer component
-    // auto quadGO = std::make_unique<GameObject>();
-    // auto quadGOTF = quadGO->addComponent<Transform>();
-    // quadGO->setName("Test Quad");
-    // quadGO->addComponent<QuadRenderer>(glm::vec4{ 1.0f, 0.0f, 0.0f, 1.0f });
-    // quadGO->addComponent<RotatorComp>();
-    // quadGOTF->rotation.y = 45.0f;
-
-    // engine.addGameObject(std::move(quadGO));
-
     if (!tryLoadSceneFromFile("scene1.json", engine)) {
         LOG_ERROR("Failed to load scene from file!");
     }
