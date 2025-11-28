1315a870dd268116e20992d46a41b9e7f8de7817
hiep.lchan <hiep.lchan@gmail.com>
2025-11-28 21:06:37 +0700

    fix QuadRenderer.h

--- engine/components/QuadRenderer.h
+++ engine/components/QuadRenderer.h
@@ -7,34 +7,32 @@
 #include <ComponentBase.h>
 
 struct Pos2D {
-    float x, y;
+    float x;
+    float y;
 };
 
 class ENGINE_EXPORT QuadRenderer : public ComponentBase<QuadRenderer> {
 public:
-    glm::vec4 color;
-
     explicit QuadRenderer(const glm::vec4& color);
     explicit QuadRenderer();
 
     ~QuadRenderer() override;
 
     void render(GameObject& owner) override;
-    void update(GameObject& owner, float deltaTime) override {}
+
+    glm::vec4 color;
+    bgfx::VertexBufferHandle m_vb{ bgfx::kInvalidHandle };
+    bgfx::IndexBufferHandle m_ib{ bgfx::kInvalidHandle };
+    bgfx::ProgramHandle m_prog{ bgfx::kInvalidHandle };
 
 #if defined(ENABLE_IMGUI)
     void onInspectorGUI() override;
 #endif
-
-private:
-    bgfx::VertexBufferHandle m_vb{ bgfx::kInvalidHandle };
-    bgfx::IndexBufferHandle m_ib{ bgfx::kInvalidHandle };
-    bgfx::ProgramHandle m_prog{ bgfx::kInvalidHandle };
 };
 
-inline constexpr ColorField<QuadRenderer> kQuadRendererColors[] = {
+inline constexpr std::array<ColorField<QuadRenderer>, 1> kQuadRendererColors = {{
     { .label = "Color", .color = &QuadRenderer::color}
-};
+}};
 
 template <>
 constexpr Property<QuadRenderer> buildMetadata<QuadRenderer>() {
